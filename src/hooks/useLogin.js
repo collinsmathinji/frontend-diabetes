dfec7a546f84529b51d2b947904488a34cf162ac
collinsmathinji <125723113+collinsmathinji@users.noreply.github.com>
2023-07-25 14:35:23 +0300

    Update useLogin.js

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('http://localhost:4000/api/user/login', {
+    const response = await fetch('https://vercel.com/collinsmathinji/diabetes-back/HbqSUGH81frver1RHnTNABg57kuy/api/user/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
